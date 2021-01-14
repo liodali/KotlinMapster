34f01acae4c590b19da39a1e3edcdb6240612d0f
dali <lionelviperdali@gmail.com>
2021-01-14 12:59:06 +0100

    optimisation in search of annotation in data class

--- src/main/kotlin/mapper/base.kt
+++ src/main/kotlin/mapper/base.kt
@@ -3,35 +3,21 @@ package mapper
 import java.lang.IllegalStateException
 import kotlin.reflect.KClass
 import kotlin.reflect.KProperty1
+import kotlin.reflect.full.declaredMemberProperties
+import kotlin.reflect.full.findAnnotation
+import kotlin.reflect.full.memberProperties
+import kotlin.reflect.full.primaryConstructor
+import kotlin.reflect.full.superclasses
 import kotlin.reflect.jvm.jvmName
 import mapper.annotations.CombineTo
 import mapper.annotations.MapTo
-import kotlin.reflect.full.*
 
+// / adaptTo : mapper from T to R
+// / runtime mapper
+// / [dest] : class type of destination object
 fun <T : Any, R : Any> T.adaptTo(dest: KClass<R>): R {
     if (dest.isData) {
-        val fieldsAnnotations = emptyList<KProperty1<Any, *>>().toMutableList()
-        val fieldsValues = HashMap<String, Any>()
-        (this::class as KClass<Any>).declaredMemberProperties.forEach { prop ->
-            if ((prop.returnType.classifier as KClass<*>).isData) {
-                (prop.returnType.classifier as KClass<*>).declaredMemberProperties.filter { subProp ->
-                    subProp.hasAnnotation<CombineTo>()
-                }.map {subProp->
-                    val annotation = subProp.findAnnotation<CombineTo>()
-                    if (annotation != null) {
-                        fieldsAnnotations.add(subProp as KProperty1<Any, *>)
-                        fieldsValues[subProp.name] = subProp.getValue(prop.get(this)!!, subProp)!!
-                    }
-                }
-
-            } else {
-                val annotation = prop.findAnnotation<CombineTo>()
-                if (annotation != null) {
-                    fieldsAnnotations.add(prop)
-                    fieldsValues[prop.name] = prop.get(this)!!
-                }
-            }
-        }
+        val (fieldsAnnotations, fieldsValues) = this.findAllAnnotationsField<CombineTo>()
         val argsValues = dest.primaryConstructor!!.parameters.map { p ->
 
             val fields = fieldsAnnotations.filter { props ->
@@ -78,7 +64,6 @@ fun <T : Any, R : Any> T.adaptTo(dest: KClass<R>): R {
             }
             v
         }.toTypedArray()
-        fieldsAnnotations.clear()
         fieldsValues.clear()
         return dest.primaryConstructor!!.call(*argsValues)
     }
@@ -94,4 +79,27 @@ fun <T : Any, R : Any> Collection<T>.adaptListTo(dest: KClass<R>): Collection<R>
     return listDest
 }
 
-
+private inline fun <reified T : Annotation> Any.findAllAnnotationsField(): Pair<List<KProperty1<Any, *>>, HashMap<String, Any>> {
+    val fieldsAnnotations = emptyList<KProperty1<Any, *>>().toMutableList()
+    val fieldsValues = HashMap<String, Any>()
+    (this::class as KClass<*>).declaredMemberProperties.forEach { prop ->
+        if ((prop.returnType.classifier as KClass<*>).isData) {
+            (prop.returnType.classifier as KClass<*>).declaredMemberProperties.filter { subProp ->
+                subProp.annotations.isNotEmpty()
+            }.map { subProp ->
+                val annotation = subProp.findAnnotation<T>()
+                if (annotation != null) {
+                    fieldsAnnotations.add(subProp as KProperty1<Any, *>)
+                    fieldsValues[subProp.name] = subProp.getValue((prop as KProperty1<Any, *>).get(this)!!, subProp)!!
+                }
+            }
+        } else {
+            val annotation = prop.findAnnotation<T>()
+            if (annotation != null) {
+                fieldsAnnotations.add(prop as KProperty1<Any, *>)
+                fieldsValues[prop.name] = prop.get(prop)!!
+            }
+        }
+    }
+    return Pair(fieldsAnnotations, fieldsValues)
+}
