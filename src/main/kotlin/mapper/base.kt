82c1a94a150202f7dfa9bb5a5955c86bec00e020
dali <lionelviperdali@gmail.com>
2021-01-14 10:51:18 +0100

    fix issues in nested annotation CombineTo

--- src/main/kotlin/mapper/base.kt
+++ src/main/kotlin/mapper/base.kt
@@ -2,22 +2,43 @@ package mapper
 
 import java.lang.IllegalStateException
 import kotlin.reflect.KClass
-import kotlin.reflect.full.declaredMemberProperties
-import kotlin.reflect.full.findAnnotation
-import kotlin.reflect.full.memberProperties
-import kotlin.reflect.full.primaryConstructor
-import kotlin.reflect.full.superclasses
+import kotlin.reflect.KProperty1
 import kotlin.reflect.jvm.jvmName
 import mapper.annotations.CombineTo
 import mapper.annotations.MapTo
+import kotlin.reflect.full.*
 
 fun <T : Any, R : Any> T.adaptTo(dest: KClass<R>): R {
     if (dest.isData) {
-        val argsValues = dest.primaryConstructor!!.parameters.map { p ->
-            val fields = (this::class as KClass<Any>).declaredMemberProperties.filter { prop ->
+        val fieldsAnnotations = emptyList<KProperty1<Any, *>>().toMutableList()
+        val fieldsValues = HashMap<String, Any>()
+        (this::class as KClass<Any>).declaredMemberProperties.forEach { prop ->
+            if ((prop.returnType.classifier as KClass<*>).isData) {
+                (prop.returnType.classifier as KClass<*>).declaredMemberProperties.filter { subProp ->
+                    subProp.hasAnnotation<CombineTo>()
+                }.map {subProp->
+                    val annotation = subProp.findAnnotation<CombineTo>()
+                    if (annotation != null) {
+                        fieldsAnnotations.add(subProp as KProperty1<Any, *>)
+                        fieldsValues[subProp.name] = subProp.getValue(prop.get(this)!!, subProp)!!
+                    }
+                }
+
+            } else {
                 val annotation = prop.findAnnotation<CombineTo>()
-                annotation?.destAtt?.equals(p.name) ?: false
+                if (annotation != null) {
+                    fieldsAnnotations.add(prop)
+                    fieldsValues[prop.name] = prop.get(this)!!
+                }
             }
+        }
+        val argsValues = dest.primaryConstructor!!.parameters.map { p ->
+
+            val fields = fieldsAnnotations.filter { props ->
+                val annotation = props.findAnnotation<CombineTo>()
+                (annotation as CombineTo).destAtt == p.name
+            }
+
             var v = if (fields.isEmpty()) {
                 val field = (this::class as KClass<Any>).declaredMemberProperties.first {
                     var name = it.name
@@ -30,19 +51,19 @@ fun <T : Any, R : Any> T.adaptTo(dest: KClass<R>): R {
                 field.get(this)
             } else {
                 if ((p.type.classifier as KClass<*>).jvmName == "java.lang.String") {
+
                     fields.sortedBy {
                         it.findAnnotation<CombineTo>()!!.index
                     }.fold("") { acc, prop ->
                         if (acc.isNotEmpty())
-                            "$acc${prop.findAnnotation<CombineTo>()!!.separator}${prop.get(this)}"
+                            "$acc${prop.findAnnotation<CombineTo>()!!.separator}${fieldsValues[prop.name]}"
                         else
-                            "${prop.get(this)}"
+                            "${fieldsValues[prop.name]}"
                     }
                 } else {
                     throw IllegalStateException("Annotation CombineTo supported only with String type")
                 }
             }
-            var v = field.get(this)
             if ((p.type.classifier as KClass<*>).isData) {
                 v = v!!.adaptTo(
                     p.type.classifier as KClass<*>
@@ -57,6 +78,8 @@ fun <T : Any, R : Any> T.adaptTo(dest: KClass<R>): R {
             }
             v
         }.toTypedArray()
+        fieldsAnnotations.clear()
+        fieldsValues.clear()
         return dest.primaryConstructor!!.call(*argsValues)
     }
     throw UnSupportedMappingType
@@ -70,3 +93,5 @@ fun <T : Any, R : Any> Collection<T>.adaptListTo(dest: KClass<R>): Collection<R>
 
     return listDest
 }
+
+
