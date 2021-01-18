4cc8d75d495cbe1915742ae940c7d1c6dadccc93
dali <lionelviperdali@gmail.com>
2021-01-18 19:50:33 +0100

    0.4.0-alpha add transformationExpression in configMapper

--- src/main/kotlin/mapper/ConfigMapper.kt
+++ src/main/kotlin/mapper/ConfigMapper.kt
@@ -1,12 +1,27 @@
 package mapper
 
 typealias ConditionalIgnore<T> = (src: T) -> Boolean
+typealias TransformationExpression<T> = (src: T) -> Any
 
+/*
+* ConfigMapper
+*
+*  give you possible to configure mapper by ignore field or ignore field in specific condition
+* or transform value of specific field
+*
+*
+*
+*
+*
+*
+ */
 class ConfigMapper<T : Any, R : Any> {
 
     internal val listIgnoredAtt: MutableList<String> = emptyList<String>().toMutableList()
     internal val listIgnoredExpression: MutableList<Pair<String, ConditionalIgnore<T>>> =
         emptyList<Pair<String, ConditionalIgnore<T>>>().toMutableList()
+    internal val listTransformationExpression: MutableList<Pair<String, TransformationExpression<T>>> =
+        emptyList<Pair<String, TransformationExpression<T>>>().toMutableList()
 
     fun ignoreAtt(srcAtt: String): ConfigMapper<T, R> {
         if (!listIgnoredAtt.contains(srcAtt))
@@ -20,6 +35,15 @@ class ConfigMapper<T : Any, R : Any> {
         }
         if (pairExist == null)
             listIgnoredExpression.add(Pair(srcAttribute, expression))
+        return this
+    }
+
+    fun transformation(srcAttribute: String, expression: TransformationExpression<T>): ConfigMapper<T, R> {
+        val pairExist = listTransformationExpression.firstOrNull {
+            it.first == srcAttribute
+        }
+        if (pairExist == null)
+            listTransformationExpression.add(Pair(srcAttribute, expression))
         return this
     }
 }
