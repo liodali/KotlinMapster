ff3d04031313455694c97d69d07bb406b3eca5f1
dali <lionelviperdali@gmail.com>
2021-01-19 18:18:50 +0100

    change data used in tests

--- src/test/kotlin/TestBaseMapper.kt
+++ src/test/kotlin/TestBaseMapper.kt
@@ -1,26 +1,25 @@
+import io.github.serpro69.kfaker.Faker
 import mapper.BaseMapper
 import mapper.ignore
 import mapper.ignoreIf
+import mapper.transformation
 import org.junit.jupiter.api.Test
 
 class TestBaseMapper {
+    val faker = Faker()
 
     @Test
     fun baseTest() {
         data class User(val name: String, val password: String)
         data class UserDTO(val name: String?, val password: String)
 
-        val user = User("hamza", password = "dali")
-        /* val config = ConfigMapper<User, UserDTO>()
-         config.ignoreIf {
-             it.name != "dali"
-         }*/
+        val name = faker.name.firstName()
+        val pwd = "1234"
+        val user = User(name, password = pwd)
+
         val mapper = BaseMapper.from(user).to(UserDTO::class)
-            .ignoreIf("name") {
-                it.name != "dali"
-            }
         val dto = mapper.adapt()
-        assert(dto == UserDTO("hamza", "dali"))
+        assert(dto == UserDTO(name = name, password = pwd))
     }
 
     @Test
@@ -28,19 +27,22 @@ class TestBaseMapper {
         data class User(val name: String, val password: String)
         data class UserDTO(val name: String?, val password: String?)
 
-        val user = User("dali", password = "1234")
+        val name = faker.name.firstName()
+
+        val user = User(name, password = "1234")
 
         val mapper = BaseMapper.from(user).to(UserDTO::class)
             .ignoreIf("name") {
-                it.name != "dali"
+                it.name != name
             }
         val dto = mapper.adapt()
         assert(dto == UserDTO(null, "1234"))
 
-        val user1 = User("dali", password = "1234")
+        val user1 = User(name, password = "1234")
         mapper.ignore("password")
         val dtoUser1 = mapper.adapt(user1)
-        assert(dtoUser1 == UserDTO("dali", null))
+        assert(dtoUser1 == UserDTO(name, null))
 
     }
+
 }
