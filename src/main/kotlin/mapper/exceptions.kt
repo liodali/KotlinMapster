acbe693987a3d650606a38b8fcc4aa7356dac2e0
dali <lionelviperdali@gmail.com>
2021-01-24 19:47:58 +0100

    0.4.0-alpha03 add new exception for ignoreIf,Transformation expression support modification of expression ignoreIf or Transformation when they are already exist

--- src/main/kotlin/mapper/exceptions.kt
+++ src/main/kotlin/mapper/exceptions.kt
@@ -3,3 +3,5 @@ package mapper
 abstract class MapperException(error: String) : Exception(error)
 
 object UnSupportedMappingType : MapperException("for now,we only support data class")
+class UnSupportedMultipleExpression(operation: String) :
+    MapperException("Unsupported multiple $operation for the same field")
