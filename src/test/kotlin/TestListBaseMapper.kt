b2cda670875cc33ba9098acdb5c25c2fc7585aef
dali <lionelviperdali@gmail.com>
2021-01-30 19:37:10 +0100

    0.4.0-alpha04 add unit test for mapping list

--- src/test/kotlin/TestListBaseMapper.kt
+++ src/test/kotlin/TestListBaseMapper.kt
@@ -1,8 +1,10 @@
 import io.github.serpro69.kfaker.Faker
 import mapper.BaseMapper
+import mapper.UndefinedSourceObject
 import mapper.adaptListTo
 import mapper.ignore
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class TestListBaseMapper {
     private val faker = Faker()
@@ -18,7 +20,6 @@ class TestListBaseMapper {
             val pwd = "1234"
             users.add(User(name, password = pwd))
         }
-        users.adaptListTo(UserDTO::class)
 
         val mapper = BaseMapper
             .fromList(users.toList())
@@ -26,5 +27,26 @@ class TestListBaseMapper {
 
         val dtoList = mapper.adaptList()
         assert(dtoList.first().name == users.first().name)
+    }
+
+    @Test
+    fun testInstanceBaseMapper() {
+        data class User(val name: String, val password: String)
+        data class UserDTO(val name: String?, val password: String?)
+
+        val users = emptyList<User>().toMutableList()
+        for (i in 0..2) {
+            val name = faker.name.firstName()
+            val pwd = "1234"
+            users.add(User(name, password = pwd))
+        }
+        val mapper = BaseMapper<User, UserDTO>().to(UserDTO::class)
+
+
+        assertThrows<UndefinedSourceObject> {
+            mapper.adaptList()
+        }
+        val listDTOs = mapper.adaptList(users)
+        assert(listDTOs.first().name==users.first().name)
     }
 }
