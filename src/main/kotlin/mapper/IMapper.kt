ec481a1421b17315e6cb6fab93fd0f01e6c35bf8
dali <lionelviperdali@gmail.com>
2021-02-08 14:46:13 +0100

    0.4.0-beta02 fix bug in adapt & adaptList IMapper extension

--- src/main/kotlin/mapper/IMapper.kt
+++ src/main/kotlin/mapper/IMapper.kt
@@ -2,17 +2,22 @@ package mapper
 
 import kotlin.reflect.KClass
 
-inline fun <reified R : Any, T : Any> IMapper<T, R>.adapt(src: T?): R {
+inline fun <reified R : Any, T : Any> IMapper<T, R>.adapt(src: T? = null): R {
     if (this is BaseMapper) {
-        return this.adapt(src)
+        return if (src != null)
+            this.adapt(src)
+        else return this.adapt()
+
     }
     return src?.adaptTo(R::class)
         ?: throw IllegalAccessException("you cannot map null object,configure BaseMapper correctly")
 }
 
-inline fun <reified R : Any, T : Any> IMapper<T, R>.adaptList(src: List<T>?): List<R> {
+inline fun <reified R : Any, T : Any> IMapper<T, R>.adaptList(src: List<T>? = emptyList()): List<R> {
     if (this is BaseMapper) {
-        return this.adaptList(src)
+        return if (src.isNullOrEmpty())
+            this.adaptList()
+        else this.adaptList(src)
     }
     return src?.adaptListTo(R::class)?.toList()
         ?: throw IllegalAccessException("you cannot map null object,configure BaseMapper correctly")
