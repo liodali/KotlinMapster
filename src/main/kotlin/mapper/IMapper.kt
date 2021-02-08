351e43a3de1fa8de5e73145eea549038661d60a8
dali <lionelviperdali@gmail.com>
2021-02-08 12:21:28 +0100

    0.4.0-beta02 create extension in IMapper to support list mapping

--- src/main/kotlin/mapper/IMapper.kt
+++ src/main/kotlin/mapper/IMapper.kt
@@ -10,6 +10,14 @@ inline fun <reified R : Any, T : Any> IMapper<T, R>.adapt(src: T?): R {
         ?: throw IllegalAccessException("you cannot map null object,configure BaseMapper correctly")
 }
 
+inline fun <reified R : Any, T : Any> IMapper<T, R>.adaptList(src: List<T>?): List<R> {
+    if (this is BaseMapper) {
+        return this.adaptList(src)
+    }
+    return src?.adaptListTo(R::class)?.toList()
+        ?: throw IllegalAccessException("you cannot map null object,configure BaseMapper correctly")
+}
+
 interface IMapper<T, R> {
 
     fun <R : Any> to(dest: KClass<R>): IMapper<*, R>
