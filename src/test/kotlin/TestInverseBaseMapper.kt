b17e973e7fff232be7dfcc8a01e659678ef5be4b
dali <lionelviperdali@gmail.com>
2021-02-17 15:19:51 +0100

    0.4.0-beta03 add new test for mapTo for inverseMapping

--- src/test/kotlin/TestInverseBaseMapper.kt
+++ src/test/kotlin/TestInverseBaseMapper.kt
@@ -1,6 +1,7 @@
 import io.github.serpro69.kfaker.Faker
 import mapper.BaseMapper
 import mapper.UndefinedDestinationObject
+import mapper.mapTo
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
@@ -21,6 +22,22 @@ class TestInverseBaseMapper {
         assert(dto == UserDTO(name = name, password = pwd))
         val orignalObject = mapper.adaptInverse(dto)
         assert(orignalObject == user)
+    }
 
+    @Test
+    fun baseMapTest() {
+        data class User(val name: String, val password: String)
+        data class UserDTO(val fullName: String?, val password: String)
+
+        val name = faker.name.firstName()
+        val pwd = "1234"
+        val user = User(name, password = pwd)
+
+        val mapper = BaseMapper.from(user).to(UserDTO::class)
+            .mapTo("name", "fullName")
+        val dto = mapper.adapt()
+        assert(dto == UserDTO(fullName = name, password = pwd))
+        val orignalObject = mapper.adaptInverse(dto)
+        assert(orignalObject == user)
     }
 }
