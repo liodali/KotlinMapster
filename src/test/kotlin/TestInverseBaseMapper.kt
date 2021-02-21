27eb716b9a739f9980bd4243571ddb69f1e50978
dali <lionelviperdali@gmail.com>
2021-02-21 13:16:11 +0100

    0.4.0-beta03 test inverseTransformation

--- src/test/kotlin/TestInverseBaseMapper.kt
+++ src/test/kotlin/TestInverseBaseMapper.kt
@@ -1,7 +1,5 @@
 import io.github.serpro69.kfaker.Faker
-import mapper.BaseMapper
-import mapper.UndefinedDestinationObject
-import mapper.mapTo
+import mapper.*
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
@@ -37,6 +35,37 @@ class TestInverseBaseMapper {
             .mapTo("name", "fullName")
         val dto = mapper.adapt()
         assert(dto == UserDTO(fullName = name, password = pwd))
+        val orignalObject = mapper.adaptInverse(dto)
+        assert(orignalObject == user)
+    }
+
+    @Test
+    fun baseInverseTransformationTest() {
+        data class User(val firstName: String, val lastName: String, val password: String)
+        data class UserDTO(val fullName: String, val password: String)
+
+        val name = faker.name.firstName()
+        val lastName = faker.name.lastName()
+        val pwd = "1234"
+        val user = User(name, lastName, password = pwd)
+
+        val mapper = BaseMapper.from(user)
+            .to(UserDTO::class).transformation(
+                "fullName"
+            ) { user ->
+                user.firstName + " " + user.lastName
+            }.inverseTransformation(
+                "firstName"
+            ) { dto ->
+                dto.fullName.split(" ").first()
+            }.inverseTransformation(
+                "lastName"
+            ) { dto ->
+                dto.fullName.split(" ").last()
+            }
+            .mapMultiple(arrayOf("firstName","lastName"), "fullName")
+        val dto = mapper.adapt()
+        assert(dto == UserDTO(fullName = "$name $lastName", password = pwd))
         val orignalObject = mapper.adaptInverse(dto)
         assert(orignalObject == user)
     }
